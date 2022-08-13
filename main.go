0c503d0e3254a09cfdf9ba976815c060aa032288
maxwellpothier <pothiermaxwell@gmail.com>
2022-08-13 17:02:41 -0600

    creates a save to file function

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
-	fmt.Println(hand.toString())
+	cards.saveToFile("my_cards")
 }
