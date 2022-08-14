19aa56f41d55153ad5965498ea707433fdce24bc
maxwellpothier <pothiermaxwell@gmail.com>
2022-08-13 19:03:31 -0600

    finishes the shuffle func

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 func main() {
 	cards := newDeck()
 
-	// hand, remainingCards := deal(cards, 5)
-
-	cards.saveToFile("my_cards")
-	newDeckFromFile("my_cards").print()
+	cards.shuffle()
+	cards.print()
 }
