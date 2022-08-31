e6707a3bd2bd5c0b9683aeb9992d7bb13fa70a38
maxwellpothier <pothiermaxwell@gmail.com>
2022-08-31 08:34:28 -0600

    adds new test for file io

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,27 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length or 52, but got %v", len(d))
 	}
+
+	if d[0] != "Two of Spades" {
+		t.Errorf("Expected first card Two of Spades, got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Ace of Clubs" {
+		t.Errorf("Expected first card Ace of Clubs, got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length or 52, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
